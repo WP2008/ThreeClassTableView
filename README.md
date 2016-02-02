ce4e589c41ae0d93f729c53daa5bfa643a41497f
汪朋 <wp1991wp@163.com>
2016-02-02 22:27:59 +0800

    create Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ThreeClassTableView
 3级tableView 选择封装  仿链家app
+
+##
---
+++ ThreeClassTableView/ThreeClassTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreeClassTableView
+//
+//  Created by WP on 16/2/2.
+//  Copyright © 2016年 xianlvke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
